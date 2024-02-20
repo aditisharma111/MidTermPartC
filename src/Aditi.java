61cc9b16ff5e85a91e8723d80f94824f88faba19
Aditi Sharma <shar1394@sheridancollege.ca>
2024-02-20 13:06:04 -0500

    added comment

---
+++ src/Aditi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aditi
+ */
+public class Aditi {
+    
+}
+
+// HII my name is aditi here i creaed a java class name aditi.
+// thank u. 
